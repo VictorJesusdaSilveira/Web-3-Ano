60b01a5c98712904d29501b5507dbe532455106d
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2026-02-11 09:17:00 -0300

    Update ex2.php

--- aula-1/ex2.php
+++ aula-1/ex2.php
@@ -7,9 +7,9 @@ function media_aritmetica($num1 , $num2 , $num3){
 }
 
 
-print media_aritmetica(10 , 15 , 20);
-print media_aritmetica(30 , 50 , 40);
-print media_aritmetica(60 , 80 , 70);
+print "Média 1: " . media_aritmetica(10 , 15 , 20) . "<br>";
+print "Média 2: " . media_aritmetica(30 , 50 , 40) . "<br>";
+print "Média 3: " . media_aritmetica(60 , 80 , 70) . "<br>";
 
 
 
