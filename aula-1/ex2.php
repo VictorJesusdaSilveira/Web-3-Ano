1fd1acddb67b49a5159cd10b889411c68cc4f344
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2026-02-11 08:26:35 -0300

    Create ex2.php

---
+++ aula-1/ex2.php
@@ -0,0 +1,16 @@
+<?php
+
+function media_aritmetica($num1 , $num2 , $num3){
+    $media = ($num1 + $num2 + $num3) / 3;
+    
+    return $media . "<br>";
+}
+
+
+print media_aritmetica(10 , 15 , 20);
+print media_aritmetica(30 , 50 , 40);
+print media_aritmetica(60 , 80 , 70);
+
+
+
+?>
