557a06031a4b927ef9fe7a7f9e461b6c370f1934
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2026-03-11 09:41:37 -0300

    Create ex_get.php

---
+++ aula-3/GET/ex_get.php
@@ -0,0 +1,34 @@
+<?php
+
+if(isset($_GET["num1"] , $_GET["num2"])){
+    $num1 = $_GET["num1"];
+    $num2 = $_GET["num2"];
+
+    $soma = $num1 + $num2;
+
+    $subtracao = $num1 -$num2;
+
+    $multiplicacao = $num1 * $num2;
+
+    $divisao = $num1 / $num2;
+
+
+    print "Soma: " . $soma . "<br>";
+
+    print "Subtração: " . $subtracao . "<br>";
+
+    print "Multiplicação: " . $multiplicacao . "<br>";
+
+    print "Divisão: " . $divisao . "<br>";
+}else{
+    print "Informe o valor dos parâmetros [num1] e [num2]";
+}
+
+//ex_get.php?num1=5&num2=5
+
+
+
+
+
+
+
