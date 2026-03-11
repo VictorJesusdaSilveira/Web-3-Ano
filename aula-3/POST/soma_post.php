76000f88d49fe3216b68d644a42f604580e5e77f
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2026-03-11 19:37:34 -0300

    Update soma_post.php

--- aula-3/POST/soma_post.php
+++ aula-3/POST/soma_post.php
@@ -1,14 +1,12 @@
 <?php
 
 $num1 = $_POST["num1"];
+$num2 = $_POST["num2"];
+$soma = $num1 + $num2;
 
 print "Esse é o primeiro número: " . $num1 . "<br>";
 
-$num2 = $_POST["num2"];
-
 print "Esse é o segundo número: " . $num2 . "<br>";
-
-$soma = $num1 + $num2;
 
 print "Essa é a soma dos números: " . $soma;
 
