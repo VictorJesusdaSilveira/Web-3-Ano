e1d017c5d51b83c56339da6bbfbca41bc27249aa
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2026-03-11 08:57:40 -0300

    Create exemplo_get.php

---
+++ aula-3/GET/exemplo_get.php
@@ -0,0 +1,17 @@
+<?php
+
+print "Parâmetros recebidos por GET: <br>";
+
+$nome = $_GET['nome'];
+
+print "Esse é o seu nome: " . $nome;
+
+print "<br>";
+
+$idade = $_GET['idade'];
+
+print "Idade de Victor: " . $idade;
+
+//exemplo_get.php?nome=Victor&idade=17
+
+?>
