cbf4aa3bfb1fc4e25079c25654e0e02ceba7b265
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2026-03-11 19:37:52 -0300

    Update exemplo_post.php

--- aula-3/POST/exemplo_post.php
+++ aula-3/POST/exemplo_post.php
@@ -1,14 +1,12 @@
 <?php
+$nome = $_POST["nome"];
+$idade = $_POST["idade"];
 
 print "Parâmetros recebidos por POST: <br>";
-
-$nome = $_POST["nome"];
 
 print "Esse é o seu nome: " . $nome;
 
 print "<br>";
-
-$idade = $_POST["idade"];
 
 print "Idade de Victor: " . $idade;
 
