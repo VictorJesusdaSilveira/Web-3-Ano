a08b59382b22c9246fab468f5f2ae569f1b1f0e8
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2026-03-11 19:38:39 -0300

    Update media_get.php

--- aula-3/GET/media_get.php
+++ aula-3/GET/media_get.php
@@ -1,18 +1,15 @@
 <?php
 
 $num1 = $_GET["num1"];
+$num2 = $_GET["num2"];
+$num3 = $_GET["num3"];
+$media = ($num1 + $num2 + $num3) / 3;
 
 print "Esse é o primeiro número: " . $num1 . "<br>";
 
-$num2 = $_GET["num2"];
-
 print "Esse é o segundo número: " . $num2 . "<br>";
 
-$num3 = $_GET["num3"];
-
 print "Esse é o terceiro número: " . $num3 . "<br>";
-
-$media = ($num1 + $num2 + $num3) / 3;
 
 print "Essa é a média aritmética dos 3 números: " . $media;
 
