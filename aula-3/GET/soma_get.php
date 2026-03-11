6477cc20dc11cecb69f2abbc63303dc85bcbdc79
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2026-03-11 08:58:31 -0300

    Add files via upload

---
+++ aula-3/GET/soma_get.php
@@ -0,0 +1,17 @@
+<?php
+
+$num1 = $_GET["num1"];
+
+print "Esse é o primeiro número: " . $num1 . "<br>";
+
+$num2 = $_GET["num2"];
+
+print "Esse é o segundo número: " . $num2 . "<br>";
+
+$soma = $num1 + $num2;
+
+print "Essa é a soma dos números: " . $soma;
+
+//soma_get.php?num1=2&num2=2
+
+?>  
